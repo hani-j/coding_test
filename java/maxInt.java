81b3c6317e9c0e2f781a702566d82dfc47b8523f
Haeun Jeong <76152041+hani-j@users.noreply.github.com>
2023-08-12 23:52:27 +0900

    day 16

---
+++ java/maxInt.java
@@ -0,0 +1,25 @@
+// 코딩테스트 연습 - 코딩테스트 입문
+// 가장 큰 수 찾기
+
+// 문제 설명
+// 정수 배열 array가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+// 제한사항
+// 1 ≤ array의 길이 ≤ 100
+// 0 ≤ array 원소 ≤ 1,000
+// array에 중복된 숫자는 없습니다.
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+class Solution {
+    public int[] solution(int[] array) {
+        int[] sort = Arrays.stream(array).sorted().toArray();
+        List<Integer> arrList = Arrays.stream(array)
+                        .boxed()
+                        .collect(Collectors.toList());;
+        int[] answer = {sort[sort.length-1], arrList.indexOf(sort[sort.length-1])};
+        return answer;
+    }
+}
