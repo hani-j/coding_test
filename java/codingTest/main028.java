25eede8e187a0ac028111db1a972a4662b03e842
Haeun Jeong <gkdms4248@gmail.com>
2023-09-29 23:50:00 +0900

    main 028

--- java/codingTest/main028.java
+++ java/codingTest/main028.java
@@ -1,35 +1,42 @@
 package codingTest;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Queue;
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class main028 {
-    static ArrayList< Edge>[] array;
-    static int[] distance;
+    static ArrayList<Edge>[] array;
     static boolean[] visited;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        array = new ArrayList[N + 1];
+    static int[] distance;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 1; i <=N; i++) {
-            array[i] = new ArrayList< Edge>();
+        int N = Integer.parseInt(st.nextToken());
+        array = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        distance = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            array[i] = new ArrayList<Edge>();
         }
         for (int i = 0; i < N; i++) {
-            int S = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
             while (true) {
-                int E = sc.nextInt();
+                int E = Integer.parseInt(st.nextToken());
                 if (E == -1)
                     break;
-                int V = sc.nextInt();
+                int V = Integer.parseInt(st.nextToken());
                 array[S].add(new Edge(E, V));
+
             }
         }
-        distance = new int[N + 1];
-        visited = new boolean[N + 1];
         BFS(1);
         int Max = 1;
         for (int i = 2; i <= N; i++) {
@@ -43,10 +50,10 @@ public class main028 {
         System.out.println(distance[N]);
     }
 
-    private static void BFS(int index) {
+    public static void BFS(int idx) {
         Queue<Integer> queue = new LinkedList<Integer>();
-        queue.add(index);
-        visited[index] = true;
+        queue.add(idx);
+        visited[idx] = true;
 
         while (!queue.isEmpty()) {
             int n = queue.poll();
@@ -60,9 +67,10 @@ public class main028 {
                 }
             }
         }
+        
     }
 
-    class Edge {
+    static class Edge {
         int e;
         int value;
 
