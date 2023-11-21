2ec58c3865edc396ff00cee46e4ee9e903258791
Haeun Jeong <gkdms4248@gmail.com>
2023-11-21 22:14:45 +0900

    stack2

---
+++ java/backjoon/stack2.java
@@ -0,0 +1,45 @@
+package backjoon;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class stack2 {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            int num = sc.nextInt();
+            if (num == 1) {
+                int n = sc.nextInt();
+                stack.push(n);
+            }
+            if (num == 2) {
+                if (stack.size() > 0)
+                    bw.write(stack.pop() + "\n");
+                else
+                    bw.write("-1\n");
+            }
+            if (num == 3)
+                bw.write(stack.size() + "\n");
+            if (num == 4){
+                if (stack.size() == 0)
+                    bw.write("1\n");
+                else
+                    bw.write("0\n");
+            }
+            if (num == 5) {
+                if (stack.size() > 0)
+                    bw.write(stack.peek() + "\n");
+                else
+                    bw.write("-1\n");
+            }
+        }
+        bw.flush();
+    }
+}
