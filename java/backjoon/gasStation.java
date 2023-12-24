f2e62726ce1a173a645de4ccb2e05c20a3036594
hani <gkdms4248@gmail.com>
2023-12-24 23:57:03 +0900

    gasStation

---
+++ java/backjoon/gasStation.java
@@ -0,0 +1,34 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+// 25-5 
+public class gasStation {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		List<Integer> distance = new ArrayList<>();
+		List<Integer> price = new ArrayList<>();
+
+		for (int i = 0; i < N - 1; i++) {
+			distance.add(Integer.parseInt(st.nextToken()));
+		}
+		for (int i = 0; i < N - 1; i++) {
+			price.add(Integer.parseInt(st.nextToken()));
+		}
+
+		int sum = 0;
+		int power = 0;
+		for (int i = 1; i < N - 1; i++) {
+		}
+
+		System.out.println(sum);
+	}
+}
