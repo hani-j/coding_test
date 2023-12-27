a5f0f1fa3619266784e9b80aa52591a866a19027
hani <gkdms4248@gmail.com>
2023-12-27 19:44:48 +0900

    modify

--- java/backjoon/colorPaper.java
+++ java/backjoon/colorPaper.java
@@ -1,80 +1,54 @@
-// package backjoon;
+package backjoon;
 
-// import java.io.BufferedReader;
-// import java.io.IOException;
-// import java.io.InputStreamReader;
-// import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-// // 26-1 2630
-// public class colorPaper {
-// 	static int[][] arr ;
-// 	public static void main(String[] args) throws IOException {
-// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-// 		int N = Integer.parseInt(br.readLine());
-// 		arr = new int[N][N];
-
-// 		for (int i = 0; i < N; i++) {
-// 			StringTokenizer st = new StringTokenizer(br.readLine());
-// 			for (int j = 0; j < N; j++) {
-// 				arr[i][j] = Integer.parseInt(st.nextToken());
-// 			}
-// 		}
-
-// 		for (int i = 0; i < )
-		
-// 	}
-	
-// 	static public int recursion(int n) {
-// 		int index = 0;
-// 	} 
-// }
-
-import java.io.*;
-import java.util.*;
-
+// 26-1 2630
 public class colorPaper {
+	static int[][] arr ;
+	static int[] color = new int[2];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
 
-    private static int[] count = new int[2];
-    private static int[][] paper;
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-    public static void main(String[] args) {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int n = Integer.parseInt(br.readLine());
-            paper = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < n; j++) {
-                    paper[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-            divide(n, 0, 0);
-            System.out.println(count[0]);
-            System.out.println(count[1]);
-        } catch (IOException ignore) {}
-    }
+		devide(0, 0, N);
 
-    public static void divide(int n, int r, int c) {
-        if (isAllNumbersSame(r, c, n)) {
-            count[paper[r][c]]++;
-        } else {
-            int length = n / 2;
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 2; j++) {
-                    divide(length, r + i * length, c + j * length);
-                }
-            }
-        }
-    }
 
-    public static boolean isAllNumbersSame(int r, int c, int length) {
-        int start = paper[r][c];
-        for (int i = r; i < r + length; i++) {
-            for (int j = c + 1; j < c + length; j++) {
-                if (paper[i][j] != start)
-                    return false;
-            }
-        }
-        return true;
-    }
+		System.out.println(color[0]);
+		System.out.println(color[1]);
+	}
+	
+	static public void devide(int s, int e, int n) {
+		if (n < 1)
+			return ;
+		if (isSquare(s, e, n)) {
+		} else {
+			devide(s, e, n/2);
+			devide(s + n/2, e, n/2);			
+			devide(s, e + n/2, n/2);
+			devide(s + n/2, e + n/2, n/2);
+		}		
+	} 
+
+	static public boolean isSquare(int s, int e, int n) {
+		int num = arr[s][e];
+		for (int i = s; i < s + n; i++) {
+			for (int j = e; j < e + n ; j++) {
+				if (arr[i][j] != num)
+					return false;
+			}  
+		}
+		color[arr[s][e]]++;
+		return true;
+	}
 }
