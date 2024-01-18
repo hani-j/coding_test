95d4c30919b41e8ab8667100b60810ee29a89904
hani <gkdms4248@gmail.com>
2024-01-18 23:48:39 +0900

    twoArray

---
+++ java/thisis/twoArray.java
@@ -0,0 +1,42 @@
+package thisis;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+// 06-03
+public class twoArray {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		int[] A = new int[N];
+		Integer[] B = new Integer[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			B[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(A);
+		Arrays.sort(B, Collections.reverseOrder());
+		
+		for (int i = 0; i < K; i++) {
+			int tmp = A[i];
+			A[i] = B[i];
+			B[i] = tmp;
+		}
+		System.out.println(Arrays.stream(A).sum());
+	}
+}
