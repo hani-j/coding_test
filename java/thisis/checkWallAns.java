c9f3fefec3a6e03e7e325e0647eeb07a9b7e7c07
Haeun Jeong <gkdms4248@gmail.com>
2024-03-08 00:29:44 +0900

    checkWallAns

--- java/thisis/checkWallAns.java
+++ java/thisis/checkWallAns.java
@@ -5,64 +5,66 @@ import java.util.List;
 
 public class checkWallAns {
 	public static int solution(int n, int[] weak, int[] dist) {
-		int length = weak.length;
-		int[] newWeak = new int[length * 2];
-		List<Integer> newDist = new ArrayList<>();
-		for (int i = 0; i < dist.length; i++) {
-			newDist.add(dist[i]);
-		}
-		for (int i = 0; i < length; i++) {
+        int length = weak.length;
+		int[] newWeak = new int[weak.length * 2];
+        for (int i = 0; i < length; i++) {
 			newWeak[i] = weak[i];
-		}
-		for (int i = 0; i < length; i++) {
-			newWeak[i + length] = weak[i] + n;
-		}
-		int answer = dist.length + 1;
+            newWeak[i + length] = weak[i] + n;
+        }
 
-		for (int i = 0; i < length; i++) {
-			List<List<Integer>> l = generateCombi(newDist, dist.length);
-			for (List<Integer> d : l) {
-				int count = 1;
-				int position = newWeak[i] + d.get(count - 1);
-				for (int j = i; j < i + length; j++) {
-					if (position < newWeak[j]) {
-						count++;
-						if (count > dist.length)
-							break;
-						position = newWeak[j] + d.get(count - 1);
-					}
-				}
-				answer = Math.min(answer, count);
-			}
-		}
-		if (answer > dist.length)
-			return -1;
-		return answer;
-	}
+        int answer = dist.length + 1;
+        for (int start = 0; start < length; start++) {
+            List<List<Integer>> permutations = generatePermutations(dist);
+            for (List<Integer> friends : permutations) {
+                int count = 1;
+                int position = newWeak[start] + friends.get(count - 1);
+                for (int index = start; index < start + length; index++) {
+                    if (position < newWeak[index]) {
+                        count++;
+                        if (count > dist.length) {
+                            break;
+                        }
+                        position = newWeak[index] + friends.get(count - 1);
+                    }
+                }
+                answer = Math.min(answer, count);
+            }
+        }
 
-	private static <T> List<List<T>> generateCombi(List<T> e, int n) {
-		List<List<T>> result = new ArrayList<>();
-		backtrack(e, 0, new ArrayList<>(), result, n);
-		return result;
-	}
+        if (answer > dist.length) {
+            return -1;
+        }
+        return answer;
+    }
 
-	private static <T> void backtrack(List<T> e, int s, List<T> c, List<List<T>> result, int n) {
-		if (c.size() == n) {
-			result.add(new ArrayList<>(c));
-			return;
-		}
+    private static List<List<Integer>> generatePermutations(int[] dist) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[dist.length];
+        backtrack(dist, used, new ArrayList<>(), result);
+        return result;
+    }
 
-		for (int i = s; i < e.size(); i++) {
-			c.add(e.get(i));
-			backtrack(e, i + 1, c, result, n);
-			c.remove(c.size() - 1);
-		}
-	}
+    private static void backtrack(int[] dist, boolean[] used, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == dist.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < dist.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                current.add(dist[i]);
+                backtrack(dist, used, current, result);
+                used[i] = false;
+                current.remove(current.size() - 1);
+            }
+        }
+    }
 
 	public static void main(String[] args) {
-		int n = 12;
-		int[] weak = {1, 5, 6, 10};
-		int[] dist = {1, 2, 3, 4};
+		// int n = 12;
+		// int[] weak = {1, 5, 6, 10};
+		// int[] dist = {1, 2, 3, 4};
 
 		// int n = 12;
 		// int[] weak = {1, 3, 4, 9, 10};
@@ -72,10 +74,67 @@ public class checkWallAns {
 		// int[] weak = {1};
 		// int[] dist = {6};
 
-		// int n = 30;
-		// int[] weak = {0, 3, 11, 21};
-		// int[] dist = {10, 4};
+		int n = 30;
+		int[] weak = {0, 3, 11, 21};
+		int[] dist = {10, 4};
 
 		System.out.println(solution(n, weak, dist));
 	}
+
+	// class Solution {
+	// 	int N = 0;
+	// 	int ans = Integer.MAX_VALUE;
+
+	// 	public int solution(int n, int[] weak, int[] dist) {
+	// 		java.util.Arrays.sort(dist);
+	// 		for (int i = 0; i < dist.length / 2; i++) {
+	// 			int temp = dist[i];
+	// 			dist[i] = dist[dist.length - 1 - i];
+	// 			dist[dist.length - 1 - i] = temp;
+	// 		}
+
+	// 		this.N = n;
+	// 		int min = Integer.MAX_VALUE;
+	// 		dfs(0, weak, dist, 0);
+
+	// 		return ans == Integer.MAX_VALUE ? -1 : ans;
+	// 	}
+
+	// 	private void dfs(int d, int[] weak, int[] dist, int visited) {
+	// 		if (visited == ((1 << weak.length) - 1)) {
+	// 			ans = Math.min(ans, d);
+	// 			return;
+	// 		}
+	// 		if (d == dist.length) {
+	// 			return;
+	// 		}
+
+	// 		for (int i = 0; i < weak.length; i++) {
+	// 			if ((visited & (1 << i)) > 0) {
+	// 				continue;
+	// 			}
+	// 			java.util.Set<Integer> set = new java.util.HashSet<>();
+	// 			for (int j = 0; j < weak.length; j++) {
+	// 				int k = (i + j) >= weak.length ? i + j - weak.length : i + j;
+	// 				if ((visited & (1 << k)) > 0) {
+	// 					continue;
+	// 				}
+	// 				int diff = weak[k] - weak[i];
+	// 				if (diff < 0) {
+	// 					diff += N;
+	// 				}
+	// 				if (diff <= dist[d]) {
+	// 					set.add(k);
+	// 					visited = visited | (1 << k);
+	// 				} else {
+	// 					break;
+	// 				}
+	// 			}
+	// 			dfs(d+1, weak, dist, visited);
+	// 			for (int v : set) {
+	// 				visited = visited & ~(1 << v);
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
