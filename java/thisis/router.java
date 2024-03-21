14b2765cbecccc46cd1ed086ec1bf24712d65b19
Haeun Jeong <gkdms4248@gmail.com>
2024-03-21 23:51:24 +0900

    router

--- java/thisis/router.java
+++ java/thisis/router.java
@@ -15,11 +15,31 @@ public class router {
 
 		int[] arr = new int[N];
 		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 		Arrays.sort(arr);
-		
-		// 1 2 0 4 0 0 0 8 9
-		// 1 2 3 1
+
+		int start = 1;
+		int end = arr[N - 1] - arr[0];
+		int result = 0;
+
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			int value = arr[0];
+			int count = 1;
+
+			for (int i = 0; i < N; i++) {
+				if (arr[i] >= value + mid) {
+					value = arr[i];
+					count++;
+				}
+			}
+			if (count >= C) {
+				start = mid + 1;
+				result = mid;
+			} else
+				end = mid - 1;
+		}
+		System.out.println(result);
 	}
 }
