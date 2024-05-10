52ea0d69090302866b34ac172606ae8cf2e47c98
Haeun Jeong <gkdms4248@gmail.com>
2024-05-10 23:51:36 +0900

    words

--- java/programmers/words.java
+++ java/programmers/words.java
@@ -31,7 +31,6 @@ public class words {
                 tNum = i;
             if (check(begin, words[i])) {
                 start.add(new Node(words[i], i));
-                System.out.println(words[i] + " " + i);
             }
         }
         for (Node n : start) {
