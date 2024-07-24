471547e2c39de0940b237990b852c37645508edd
Haeun Jeong <gkdms4248@gmail.com>
2024-07-24 22:02:09 +0900

    Tile

---
+++ java/programmers/Tile.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class Tile {
+	static public int solution(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+		for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+        return dp[n];
+    }
+	
+	public static void main(String[] args) {
+		int n = 4;
+		System.out.println(solution(n));
+	}
+}
