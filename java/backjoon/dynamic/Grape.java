9c7bb51d18f09affcdf085a3d9daf09d9d84cfc6
Haeun Jeong <gkdms4248@gmail.com>
2024-08-23 23:03:15 +0900

    Grape

--- java/backjoon/dynamic/Grape.java
+++ java/backjoon/dynamic/Grape.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Graph {
+public class Grape {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
