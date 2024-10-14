8674d5f938d29352660c513136e8f793aed57010
Haeun Jeong <gkdms4248@gmail.com>
2024-10-14 23:36:38 +0900

    Scale

---
+++ java/backjoon/greedy/Scale.java
@@ -0,0 +1,33 @@
+package backjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Scale {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] weight = new int[N];
+		for (int i = 0; i < N; i++)
+			weight[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(weight);
+		if (weight[0] != 1) {
+			System.out.println(1);
+			return ;
+		}
+		int sum = weight[0];
+		for (int i = 1; i < N; i++) {
+			if (weight[i] > sum + 1) {
+				System.out.println(sum + 1);
+				return;
+			}
+			sum += weight[i];
+		}
+		System.out.println(sum + 1);
+	}
+}
